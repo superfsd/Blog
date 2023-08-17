9be1b8315283e90e8007f92ed6f10b39dc8e01d1
Yi Xu5 <Yi_Xu5@jabil.com>
2023-08-17 16:56:34 +0800

    feat:添加文件

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: '技术博客',
+  description: '个人技术博客',
   base: '/Blog/'
 })
---
+++ docs/guide/nuget-guide.md
@@ -0,0 +1,6 @@
+# 创建包
+## 基于Net
+1.使用vS生成Nuget包
+
+
+## 基于NET Framework
